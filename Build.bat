49145e127f8a4da44dd90aa1376be95c78ed3570
pmengal_cp <SND\pmengal_cp>
2009-12-12 13:04:22 +0000

    Added build batch + documentation

---
+++ Build.bat
@@ -0,0 +1,4 @@
+%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.sln
+%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.Compact.sln
+%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe Build\Documentation.shfbproj
+PAUSE
