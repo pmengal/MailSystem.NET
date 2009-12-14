946d6c4efaac71352b7b5554c33c9377b52023d3
pmengal_cp <SND\pmengal_cp>
2009-12-14 19:47:28 +0000

    Fixed Build.bat Temporally removed compact framework build

--- Build.bat
+++ Build.bat
@@ -1,7 +1,7 @@
 echo Compile
 
 %SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.sln /p:Configuration=Retail
-%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.Compact.sln /p:Configuration=Retail
+rem %SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.Compact.sln /p:Configuration=Retail
 %SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe Build\Documentation.shfbproj /p:Configuration=Release
 
 echo Prepare Release
@@ -11,8 +11,9 @@ MKDIR Release
 :NOWINDIR
 
 Del Release\*.* /Q
-XCOPY "Class Library\ActiveUp.Net.Mail\bin\Release\*.xml" Release\
-XCOPY "Class Library\ActiveUp.Net.Mail\bin\Release\*.dll" Release\
+XCOPY "Class Library\ActiveUp.Net.Mail\bin\Retail\*.xml" Release\
+XCOPY "Class Library\ActiveUp.Net.Mail\bin\Retail\*.dll" Release\
 XCOPY Samples Release\Samples /s /i
 
 PAUSE
+
