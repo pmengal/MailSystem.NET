e2d8bbcf924c3423afd5ae3a5fc2d68f92a387ad
pmengal_cp <SND\pmengal_cp>
2009-12-14 15:23:49 +0000

    Added a requirement Updated build batch

--- Build.bat
+++ Build.bat
@@ -1,4 +1,18 @@
-%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.sln
-%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.Compact.sln
-%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe Build\Documentation.shfbproj
+echo Compile
+
+%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.sln /p:Configuration=Retail
+%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe ActiveUp.Net.Compact.sln /p:Configuration=Retail
+%SystemRoot%\Microsoft.NET\Framework\v3.5\MSBuild.exe Build\Documentation.shfbproj /p:Configuration=Release
+
+echo Prepare Release
+
+IF EXIST Release GOTO NOWINDIR
+MKDIR Release
+:NOWINDIR
+
+Del Release\*.* /Q
+XCOPY "Class Library\ActiveUp.Net.Mail\bin\Release\*.xml" Release\
+XCOPY "Class Library\ActiveUp.Net.Mail\bin\Release\*.dll" Release\
+XCOPY Samples Release\Samples /s /i
+
 PAUSE
