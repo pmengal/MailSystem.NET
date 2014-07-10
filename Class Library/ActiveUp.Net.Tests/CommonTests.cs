2cb07a39c72614029ccf3cea08a68c04b5dfd853
MZimmermann_cp <SND\MZimmermann_cp>
2014-07-10 16:40:46 +0000

    applied patch #16589

--- Class Library/ActiveUp.Net.Tests/CommonTests.cs
+++ Class Library/ActiveUp.Net.Tests/CommonTests.cs
@@ -1,24 +1,30 @@
 ﻿using System;
-
 using ActiveUp.Net.Mail;
 using NUnit.Framework;
 
 namespace ActiveUp.Net.Tests
 {
     /// <summary>
-    /// ActiveUp.Net.Common library related tests.
+    ///    ActiveUp.Net.Common library related tests.
     /// </summary>
     [TestFixture(Description = "ActiveUp.Net.Common library related tests")]
     public class CommonTests
     {
-        [Test(Description = "Verify the correct parsing of multiple trace entries in a mail and use the latest one as message.ReceivedDate")]
-        public void ParseMultipleMessageTrace()
+        [Test(Description = "Verify the correct parsing of multiple trace entries in a mail and let not affect the message.Date")]
+        public void ParseMultipleMessageTraceMessageDate()
         {
-            Message message = new Message();
-            message = Parser.ParseMessageFromFile("resource\\testmail.eml");
-            // TODO: The code seems legit, is the test wrong?
-            Assert.AreEqual(message.ReceivedDate, new DateTime(2013, 01, 06, 07, 07, 07));
-            Assert.AreEqual(message.Date, new DateTime(2013, 01, 04, 03, 04, 04));
+            DateTime expected = new DateTime(2013, 01, 04, 03, 04, 04);
+            Message message = Parser.ParseMessageFromFile("resource\\testmail.eml");
+            Assert.AreEqual(expected, message.Date);
+        }
+
+        [Test(Description = "Verify the correct parsing of multiple trace entries in a mail and use the latest one as message.ReceivedDate")]
+        public void ParseMultipleMessageTraceReceivedDate()
+        {
+            //relevant entry is "Mon, 7 Jan 2013 07:07:07 +0100" so Parser.ParseAsUniversalDateTime will give the following datetime as Result
+            DateTime expected = new DateTime(2013, 01, 07, 06, 07, 07);
+            Message message = Parser.ParseMessageFromFile("resource\\testmail.eml");
+            Assert.AreEqual(expected, message.ReceivedDate);
         }
     }
 }
